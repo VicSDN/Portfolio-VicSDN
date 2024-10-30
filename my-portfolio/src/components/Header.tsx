407ecd0e4fb9dae4367a60e6857ba3e7070c911b
VicSDN <victormenendez2011@gmail.com>
2024-10-30 13:49:16 +0100

    Finish Aboutme page in desing web

--- my-portfolio/src/components/Header.tsx
+++ my-portfolio/src/components/Header.tsx
@@ -1,34 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-import i18n from "../i118n/i118n"; 
+import i18n from "../i118n/i118n";
 import { Link } from "react-router-dom";
+import SpanishFlag from "../assets/SpanishFlag.svg";
+import EnglishFlag from "../assets/EnglishFlag.svg";
 
 const Header: React.FC = () => {
   const { t } = useTranslation();
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [currentLanguage, setCurrentLanguage] = useState("es");
+
+  const handleLanguageChange = (lang: string) => {
+    i18n.changeLanguage(lang);
+    setCurrentLanguage(lang);
+    setDropdownOpen(false);
+  };
 
   return (
-    <header className="fixed top-0 right-0 w-[50%] h-9 shadow-lg">
-      <div className="p-4  bg-slate-600 flex flex-row">
-        <nav className="mt-4 ">
-          <ul className="mt-4 flex flex-row">
-            <li>
-              <Link to="/" className="text-lg">
-                {t("aboutMe.title")}
-              </Link>
-            </li>
-            <li>
-              <Link to="/projects" className="text-lg">
-                {t("projects.title")}
-              </Link>
-            </li>
-          </ul>
-        </nav>
-
-        <div className="mt-4">
-          <button onClick={() => i18n.changeLanguage("es")}>Español</button>
-          <button onClick={() => i18n.changeLanguage("en")}>English</button>
+    <header className="fixed top-0 right-0 w-[50%] h-16 shadow-lg bg-navy-blue z-10 flex items-center justify-center">
+      <nav className="flex items-center space-x-8 gap-8">
+        <ul className="flex space-x-8 gap-8">
+          <li>
+            <Link
+              to="/"
+              className="text-lg text-deep-dark-blue hover:text-sky-blue"
+            >
+              {t("aboutMe.title")}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/projects"
+              className="text-lg text-deep-dark-blue hover:text-sky-blue"
+            >
+              {t("projects.title")}
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/achievements"
+              className="text-lg text-deep-dark-blue hover:text-sky-blue"
+            >
+              {t("achievements.title")}
+            </Link>
+          </li>
+        </ul>
+        <div className="relative">
+          <button
+            onClick={() => setDropdownOpen(!dropdownOpen)}
+            className="text-lg text-deep-dark-blue hover:text-sky-blue focus:outline-none"
+          >
+            <img
+              src={currentLanguage === "es" ? SpanishFlag : EnglishFlag}
+              alt={currentLanguage === "es" ? "SpanishFlag" : "EnglishFlag"}
+              className="inline-block w-8 h-8 rounded-full"
+            />
+          </button>
+          {dropdownOpen && (
+            <ul className="absolute right-0 mt-2 w-40 bg-white shadow-lg rounded-md z-10 border border-gray-200">
+              <li className="hover:bg-gray-200 hover:border hover:border-blue-500 rounded-md">
+                <button
+                  onClick={() => handleLanguageChange("es")}
+                  className="flex items-center w-full px-4 py-2 text-left text-gray-800"
+                >
+                  <img
+                    src={SpanishFlag}
+                    alt="Español"
+                    className="w-5 h-5 mr-2"
+                  />
+                  Español
+                </button>
+              </li>
+              <li className="hover:bg-gray-200 hover:border hover:border-blue-500 rounded-md">
+                <button
+                  onClick={() => handleLanguageChange("en")}
+                  className="flex items-center w-full px-4 py-2 text-left text-gray-800"
+                >
+                  <img
+                    src={EnglishFlag}
+                    alt="English"
+                    className="w-5 h-5 mr-2"
+                  />
+                  English
+                </button>
+              </li>
+            </ul>
+          )}
         </div>
-      </div>
+      </nav>
     </header>
   );
 };
--- my-portfolio/src/pages/AboutMe.tsx
+++ my-portfolio/src/pages/AboutMe.tsx
@@ -1,14 +1,25 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import myPhoto from "../assets/myphoto.webp";
 
 const AboutMe: React.FC = () => {
   const { t } = useTranslation();
-
   return (
-    <div>
-      <h1 className="text-2xl font-bold">{t("aboutMe.title")}</h1>
-      <p className="mt-2">{t("aboutMe.description")}</p>
-    </div>
+    <section className="fixed top-16 right-0 w-1/2 h-full bg-deep-dark-blue p-4">
+      <div className="flex flex-col items-center">
+        <img
+          src={myPhoto}
+          alt="myphoto"
+          className="w-auto h-auto max-w-full max-h-48 mb-4 z-50 transition-transform transform hover:scale-150"
+        />
+      </div>
+      <h1 className="text-3xl text-black font-title font-bold italic text-left mb-2">
+        {t("aboutMe.title")}
+      </h1>
+      <p className="text-base text-white font-body italic leading-relaxed">
+        {t("aboutMe.description")}
+      </p>
+    </section>
   );
 };
 
