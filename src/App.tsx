5ef25fca902ba50c7e1514a89c0141a8de591c66
VicSDN <victormenendez2011@gmail.com>
2024-12-03 23:52:48 +0100

    Changes in app for deploy

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,17 @@ import Header from "./components/Header";
 import AboutMe from "./pages/AboutMe";
 import Projects from "./pages/Projects";
 import Achievements from "./pages/Achievements";
+import { Navigate } from "react-router-dom";
 
 const App: React.FC = () => {
   return (
     <Router basename="/Portfolio-VicSDN">
-    
       <SocialMedia />
       <Header />
       <main className="ml-1/3 p-4">
         <Routes>
-          <Route path="/" element={<AboutMe />} />
+          <Route path="/" element={<Navigate to="/aboutMe" />} />
+          <Route path="/aboutMe" element={<AboutMe />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/achievements" element={<Achievements />} />
         </Routes>
