86351ba3c5d3373f5a0ddf10b0d8dd3590b4d546
Víctor Manuel Menéndez López <151613334+VicSDN@users.noreply.github.com>
2024-12-17 12:26:55 +0000

    Changes header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,19 +17,22 @@ const Header: React.FC = () => {
   return (
     <header className="fixed top-0 right-0 w-full lg:w-[50%] h-16 shadow-lg bg-navy-blue z-10 flex items-center justify-between px-4 sm:px-8">
       <div className="flex items-center justify-between w-full">
+        {/* Botón hamburguesa en pantallas pequeñas */}
         <button
-          className="lg:hidden text-white text-2xl"
+          className="sm:hidden text-white text-2xl"
           onClick={() => setMenuOpen(!menuOpen)}
         >
           {menuOpen ? "×" : "☰"}
         </button>
 
+        {/* Menú de navegación, solo muestra hamburguesa en pantallas pequeñas */}
         <nav
           className={`${
             menuOpen ? "block" : "hidden"
-          } lg:flex lg:space-x-8 items-center flex-col lg:flex-row w-full lg:w-auto`}
+          } md:flex lg:space-x-8 items-center flex-row md:flex-row w-full lg:w-auto`}
         >
-          <ul className="space-y-4 lg:space-y-0 lg:flex lg:space-x-8">
+          {/* Menú horizontal */}
+          <ul className="space-y-4 lg:space-y-0 lg:flex lg:space-x-8 justify-center lg:justify-between w-full md:w-full flex space-x-8">
             <li>
               <Link to="/" className="text-lg text-white hover:text-sky-blue">
                 {t("aboutMe.title")}
@@ -52,6 +55,8 @@ const Header: React.FC = () => {
               </Link>
             </li>
           </ul>
+
+          {/* Selector de idioma */}
           <div className="relative mt-4 lg:mt-0">
             <button
               onClick={() => setDropdownOpen(!dropdownOpen)}
