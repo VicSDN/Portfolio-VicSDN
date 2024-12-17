c580f46d6a14b9dcecef25e1cd5d6f8dfe3ba194
Víctor Manuel Menéndez López <151613334+VicSDN@users.noreply.github.com>
2024-12-17 10:21:38 +0000

    Changes in styles in achievements

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,16 +11,16 @@ const Card: React.FC<CardProps> = ({ image, title, onClick }) => {
 
   return (
     <div
-      className="w-[75%] h-48 sm:h-60 bg-cover bg-center rounded-lg shadow-lg cursor-pointer transition-transform transform hover:scale-105"
+      className="w-[75%] max-w-md h-48 sm:h-60 bg-cover bg-center rounded-lg shadow-lg cursor-pointer transition-transform transform hover:scale-105"
       onClick={onClick}
     >
       <img
         src={imageSrc} 
         alt={title}
-        className="absolute inset-0 w-full h-full object-cover rounded-lg"
+        className="absolute inset-0 w-full h-full  object-cover rounded-lg"
       />
-      <div className="absolute inset-0 bg-black bg-opacity-50 rounded-lg flex items-center justify-center">
-        <h3 className="text-white text-lg font-bold text-center">{title}</h3>
+      <div className="absolute inset-0 bg-black bg-opacity-30 rounded-lg flex items-end justify-center">
+        <h3 className="text-white text-opacity-90 tracking-wide text-base leading-relaxed  p-2 m-2 rounded-md bg-gradient-to-br from-blue-100 via-blue-950 to-cyan-950 p-1">{title}</h3>
       </div>
     </div>
   );
