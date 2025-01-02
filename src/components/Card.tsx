911dac029c9c72de1821b00302aba9a84e476399
Víctor Manuel Menéndez López <151613334+VicSDN@users.noreply.github.com>
2025-01-02 23:09:32 +0000

    Changes en achievemetns page

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,28 +1,35 @@
 import React from "react";
 
 interface CardProps {
-  image: string | string[];
+  image: string;
   title: string;
-  onClick?: () => void;
+  description: string;
+  category: string;
 }
 
-const Card: React.FC<CardProps> = ({ image, title, onClick }) => {
-  const imageSrc = Array.isArray(image) ? image[0] : image;
-
+const Card: React.FC<CardProps> = ({ image, title, description , category}) => {
   return (
-    <div
-      className="w-[75%] max-w-md h-48 sm:h-60 bg-cover bg-center rounded-lg shadow-lg cursor-pointer transition-transform transform hover:scale-105"
-      onClick={onClick}
-    >
+    <a href="#" className="group relative block bg-black">
       <img
-        src={imageSrc} 
         alt={title}
-        className="absolute inset-0 w-full h-full  object-cover rounded-lg"
+        src={image}
+        className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
       />
-      <div className="absolute inset-0 bg-black bg-opacity-30 rounded-lg flex items-end justify-center">
-        <h3 className="text-white text-opacity-90 tracking-wide text-base leading-relaxed  p-2 m-2 rounded-md bg-gradient-to-br from-blue-100 via-blue-950 to-cyan-950 p-1">{title}</h3>
+
+      <div className="relative p-4 sm:p-6 lg:p-8">
+        <p className="text-sm font-medium uppercase tracking-widest text-pink-500">
+          {category}
+        </p>
+
+        <p className="text-xl font-bold text-white sm:text-2xl">{title}</p>
+
+        <div className="mt-32 sm:mt-48 lg:mt-64">
+          <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+            <p className="text-sm text-white">{description}</p>
+          </div>
+        </div>
       </div>
-    </div>
+    </a>
   );
 };
 
--- src/pages/Achievements.tsx
+++ src/pages/Achievements.tsx
@@ -1,159 +1,82 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
 import Card from "../components/Card";
-import Modal from "../components/Modal";
 
 interface Achievement {
   image: string | string[];
   title: string;
   description: string;
   projectImages?: string[];
+  category: string;
 }
 
 const Achievements: React.FC = () => {
   const { t } = useTranslation("achievements");
-  const achievements =
-    (t("achievements.list", { returnObjects: true }) as Achievement[]) || [];
+  const achievements = (t("achievements.list", { returnObjects: true }) as Achievement[]) || [];
 
-  const [selectedAchievementIndex, setSelectedAchievementIndex] = useState<
-    number | null
-  >(null);
   const [currentProjectImageIndex, setCurrentProjectImageIndex] = useState(0);
   const [imageLoaded, setImageLoaded] = useState(false);
 
-  const handleModalOpen = (index: number) => {
-    setSelectedAchievementIndex(index);
-    setCurrentProjectImageIndex(0);
-    setImageLoaded(false);
-  };
-
-  const handleModalClose = () => {
-    setSelectedAchievementIndex(null);
-    setCurrentProjectImageIndex(0);
-    setImageLoaded(false);
-  };
-
   const handleNextProjectImage = () => {
-    if (
-      selectedAchievementIndex !== null &&
-      achievements[selectedAchievementIndex].projectImages
-    ) {
-      const projectImages =
-        achievements[selectedAchievementIndex].projectImages;
-      if (projectImages.length > 1) {
-        setCurrentProjectImageIndex(
-          (prevIndex) => (prevIndex + 1) % projectImages.length
-        );
-      }
-    }
+    setCurrentProjectImageIndex((prevIndex) => prevIndex + 1);
   };
 
   const handlePrevProjectImage = () => {
-    if (
-      selectedAchievementIndex !== null &&
-      achievements[selectedAchievementIndex].projectImages
-    ) {
-      const projectImages =
-        achievements[selectedAchievementIndex].projectImages;
-      if (projectImages.length > 1) {
-        setCurrentProjectImageIndex(
-          (prevIndex) =>
-            (prevIndex - 1 + projectImages.length) % projectImages.length
-        );
-      }
-    }
+    setCurrentProjectImageIndex((prevIndex) => (prevIndex - 1 + achievements.length) % achievements.length);
   };
 
   return (
     <section className="fixed top-16 right-0 w-full md:w-1/2 h-full bg-deep-dark-blue p-6 overflow-y-auto">
       <span className="p-2 flex flex-col items-start my-2">
-      <h1 className="text-2xl text-neutral-600 font-title font-bold mb-4">
-        {t("achievements.title")}
-      </h1>
-      <p className="text-white text-opacity-90 tracking-wide text-base leading-relaxed ">
-        {t("achievements.description")}
-      </p>
+        <h1 className="text-2xl text-neutral-600 font-title font-bold mb-4">
+          {t("achievements.title")}
+        </h1>
+        <p className="text-white text-opacity-90 tracking-wide text-base leading-relaxed ">
+          {t("achievements.description")}
+        </p>
       </span>
-      <div className="grid grid-cols-1 place-items-center sm:grid-cols-1 gap-6">
+
+      <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 gap-6">
         {achievements.map((achievement, index) => (
           <Card
             key={index}
             image={Array.isArray(achievement.image) ? achievement.image[0] : achievement.image}
             title={achievement.title}
-            onClick={() => handleModalOpen(index)}
+            description={achievement.description}
+            category={achievement.category}
           />
         ))}
       </div>
 
-      {selectedAchievementIndex !== null && (
-        <Modal onClose={handleModalClose}>
-          <div className="flex flex-col items-center gap-4">
-            <h2 className="text-2xl text-white font-title font-bold">
-              {achievements[selectedAchievementIndex].title}
-            </h2>
-            <div className="relative w-full max-w-lg">
-              <img
-                src={Array.isArray(achievements[selectedAchievementIndex].image)
-                  ? achievements[selectedAchievementIndex].image[0]
-                  : achievements[selectedAchievementIndex].image}
-                alt={`Imagen del logro`}
-                className={`w-full rounded-lg ${imageLoaded ? "" : "hidden"}`}
-                onLoad={() => setImageLoaded(true)} 
-              />
-              {!imageLoaded && <p className="text-white">Cargando imagen...</p>}
-            </div>
-
-            <p className="text-white text-opacity-90 text-center mt-4">
-              {achievements[selectedAchievementIndex].description}
-            </p>
-
-            {achievements[selectedAchievementIndex].projectImages && (
+      {achievements.length > 0 && achievements[0]?.projectImages && (
+        <div className="flex flex-col items-center gap-4 mt-6">
+          <h3 className="text-xl text-white">Imágenes del Proyecto</h3>
+          <div className="relative w-full max-w-lg mt-4">
+            <img
+              src={achievements[0].projectImages[currentProjectImageIndex]}
+              alt={`Imagen del proyecto ${currentProjectImageIndex + 1}`}
+              className={`w-full rounded-lg ${imageLoaded ? "" : "hidden"}`}
+              onLoad={() => setImageLoaded(true)}
+            />
+            {!imageLoaded && <p className="text-white">Cargando imagen...</p>}
+            {achievements[0].projectImages.length > 1 && (
               <>
-                <h3 className="text-xl text-white mt-6">Imágenes del Proyecto</h3>
-                <div className="relative w-full max-w-lg mt-4">
-                  <img
-                    src={achievements[selectedAchievementIndex].projectImages[currentProjectImageIndex]}
-                    alt={`Imagen del proyecto ${currentProjectImageIndex + 1}`}
-                    className={`w-full rounded-lg ${imageLoaded ? "" : "hidden"}`}
-                    onLoad={() => setImageLoaded(true)} 
-                  />
-                  {!imageLoaded && <p className="text-white">Cargando imagen...</p>}
-                  {achievements[selectedAchievementIndex].projectImages.length > 1 && (
-                    <>
-                      <button
-                        onClick={handlePrevProjectImage}
-                        className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white/30 p-2 rounded-full hover:bg-white"
-                      >
-                        ◀
-                      </button>
-                      <button
-                        onClick={handleNextProjectImage}
-                        className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white/30 p-2 rounded-full hover:bg-white"
-                      >
-                        ▶
-                      </button>
-                    </>
-                  )}
-                </div>
+                <button
+                  onClick={handlePrevProjectImage}
+                  className="absolute top-1/2 left-2 transform -translate-y-1/2 bg-white/30 p-2 rounded-full hover:bg-white"
+                >
+                  ◀
+                </button>
+                <button
+                  onClick={handleNextProjectImage}
+                  className="absolute top-1/2 right-2 transform -translate-y-1/2 bg-white/30 p-2 rounded-full hover:bg-white"
+                >
+                  ▶
+                </button>
               </>
             )}
-
-            <div className="flex gap-4 mt-4">
-              <button
-                onClick={() => setSelectedAchievementIndex(prevIndex => prevIndex !== null ? (prevIndex - 1 + achievements.length) % achievements.length : 0)}
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-              >
-                {t("previous")}
-              </button>
-              <button
-                onClick={() => setSelectedAchievementIndex(prevIndex => prevIndex !== null ? (prevIndex + 1) % achievements.length : 0)}
-                className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-              >
-                {t("next")}
-              </button>
-            </div>
           </div>
-        </Modal>
+        </div>
       )}
     </section>
   );
